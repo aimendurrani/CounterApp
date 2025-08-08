024393566f36b809939789cea3f21b9232d02ce6
Aimen Durrani <130763335+aimendurrani@users.noreply.github.com>
2025-08-08 16:19:13 +0500

    Initial commit: Angular counter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counterApp';
+  count=0
+
+  handleCounter(val:string){
+    if(val==="minus"){
+      this.count = this.count-1
+    }else if(val==="plus"){
+       this.count = this.count+1
+    }else if(val==="zero"){
+       this.count = 0
+    }
+  }
 }
